1a64292184b8f6dc738c0a322a264dfd0a209af9
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-11-24 17:39:01 +0000

    Add ability to input location and range

--- src/index.js
+++ src/index.js
@@ -26,9 +26,9 @@ import "./styles.css"
 //         console.error(err)
 //     })
 
-    async function getWeatherData(range){
+    async function getWeatherData(range, location){
         try{
-            const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london/?key=L294L6C244ZLCSJV32VN44K6R', {mode:'cors'})
+            const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=L294L6C244ZLCSJV32VN44K6R`, {mode:'cors'})
             const data = await response.json()
             const weatherDataArray = []
             data.days.slice(0,range).forEach(day => {
@@ -45,15 +45,28 @@ import "./styles.css"
                     tempmax: day.tempmax
                 }
                 weatherDataArray.push(weatherData)
-                console.log(weatherDataArray)
-                return weatherDataArray
+                
             });
+            console.log(weatherDataArray)
+            return weatherDataArray
         } catch (error){
             console.log(error)
             const body = document.querySelector("body")
             body.classList.add("errorPage")
             body.innerHTML = "Sorry not working right now"
         }
+
     }
 
-    getWeatherData(2)
+const locationForm = document.querySelector("#locationForm")
+const location = document.querySelector("#location")
+const range = document.querySelector("#range")
+locationForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const locationValue = location.value 
+    const rangeValue = range.value
+    console.log(locationValue)
+    console.log(rangeValue)
+    getWeatherData(rangeValue, locationValue)
+})
+
